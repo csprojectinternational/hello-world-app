f4939462a147ea9377cf394d344a20283bec53ee
ericli3690 <ericli3690@gmail.com>
2023-02-04 16:44:28 -0700

    created textbox for testing purposes added to app.js created components folder

--- hello-world-react/src/App.js
+++ hello-world-react/src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Textbox from './components/Textbox/Textbox';
 import './App.css';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         >
           Damn that's a pretty spooky ghost! 😱
         </a>
+        <Textbox />
       </header>
     </div>
   );
---
+++ hello-world-react/src/components/Textbox/Textbox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Textbox.css"
+
+const Textbox = () => {
+  return (
+    <div>
+      <h3 className="Main-text">This is a textbox</h3>
+    </div>
+  )
+}
+
+export default Textbox
