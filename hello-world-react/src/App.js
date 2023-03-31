f9898d3425010ff0e0407a4a6fd1dbdc620f111e
ericli3690 <ericli3690@gmail.com>
2023-03-31 17:14:21 -0600

    removed testing files reset project and deleted stock files created from create-react-app renamed everything to suit the branding of "tied together" added new favicon

--- hello-world-react/src/App.js
+++ hello-world-react/src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
-import Textbox from './components/Textbox/Textbox';
 import './App.css';
+
+// ROOT
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          INTRODUCING PULSAR
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          A new artificial intelligence startup featuring deep machine learning, neural networks, and state of the art blockchain technologies to invest in non-fungible technologies,
-          receiving a 4B Series C Valuation
-        </a>
-        <Textbox />
-      </header>
+      <p>This is some text.</p>
     </div>
   );
 }
