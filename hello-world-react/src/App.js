33d898a73c37e7a7a6cb8fd4dd6893b0e3a86e2a
ericli3690 <ericli3690@gmail.com>
2023-03-31 17:41:21 -0600

    implemented basic react-router in app.js

--- hello-world-react/src/App.js
+++ hello-world-react/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // router
+
 // ROOT
 
 function App() {
   return (
     <div className="App">
-      <p>This is some text.</p>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<p>Onboarding</p>} />
+          <Route exact path="/register" element={<p>Register</p>} />
+          <Route exact path="/dashboard" element={<p>Dashboard</p>} />
+          <Route path="*" element={<p>404: This page does not exist!</p>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
