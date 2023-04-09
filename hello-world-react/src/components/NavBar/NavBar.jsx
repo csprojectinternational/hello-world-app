38a9ea24a61929b3da7ec65c150fb5ee7cec26df
ericli3690 <ericli3690@gmail.com>
2023-04-08 22:57:56 -0600

    working on registration 1. fixed bug in manifest 2. renamed some routes in root 3. added register.jsx

--- hello-world-react/src/components/NavBar/NavBar.jsx
+++ hello-world-react/src/components/NavBar/NavBar.jsx
@@ -3,12 +3,13 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import { Avatar } from '@mui/material'
+import './logo.png'
 
 const NavBar = () => {
   return (
     <AppBar position="sticky" sx={{ backgroundColor: 'lightgrey', boxShadow: "0px 0px" }}>
       <Toolbar>
-        <img src="./logo.png" alt="logo" className="logo" style={{ height: '2.5rem', paddingRight: '1rem' }} />
+        <img src="logo.png" alt="logo" className="logo" style={{ height: '2.5rem', paddingRight: '1rem' }} />
         <Typography variant="h4" sx={{ fontWeight: 700, color: 'darkred' }}>
           TIED TOGETHER
         </Typography>
--- hello-world-react/src/pages/ErrorPage/ErrorPage.jsx
+++ hello-world-react/src/pages/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Typography from '@mui/material/Typography'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div>ErroPage</div>
+    <div style={{position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)"}}>
+      <Typography variant="h4">404: Sorry, an error occured!</Typography>
+      <Link to="/">
+        <button style={{backgroundColor: "white", padding: "15px", margin: "25px", fontFamily: "Montserrat"}}>Return to Home</button>
+      </Link>
+    </div>
   )
 }
 
