9108a518f04a8663f9620a03293569b76962fed3
pranavUl <pranavullas46@gmail.com>
2023-04-18 22:24:38 -0500

    attempted railway error fix

--- hello-world/src/main/java/com/csi/helloworld/Tutor/TutorService.java
+++ hello-world/src/main/java/com/csi/helloworld/Tutor/TutorService.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("TutorService")
 public class TutorService {
     @Autowired
     private TutorRepository tutorRepository;
