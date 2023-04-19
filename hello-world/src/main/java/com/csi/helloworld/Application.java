ec958c7b067a0e6a1d390b6d0a636c12eada007c
pranavUl <pranavullas46@gmail.com>
2023-04-18 19:06:01 -0500

    DOCKER, MAVEN BUILD WORKS

--- hello-world/src/main/java/com/csi/helloworld/Application.java
+++ hello-world/src/main/java/com/csi/helloworld/Application.java
@@ -2,12 +2,13 @@ package com.csi.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @RestController
 public class Application {
 	public static void main(String[] args) {
